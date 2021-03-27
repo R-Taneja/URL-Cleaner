3ed508f89d5d966df48ab52cd8d9e172499a9020
Rohan Taneja <47066511+R-Taneja@users.noreply.github.com>
2021-03-27 18:40:29 -0400

    Finished UI

---
+++ url_cleaner/lib/home.dart
@@ -0,0 +1,87 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:js' as js;
+
+import 'package:flutter/material.dart';
+
+import 'styles.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "URL Cleaner",
+              style: headerStyle,
+            ),
+            // TODO: fix the text overflow on mobile
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "A tool to clean URLs before you share them. Inspired by ",
+                    style: bodyTextStyle,
+                  ),
+                  TextButton(
+                    child: Text(
+                      "Jordan Singer",
+                      style: linkTextStyle,
+                    ),
+                    onPressed: () {
+                      js.context
+                          .callMethod('open', ['https://twitter.com/jsngr']);
+                    },
+                  ),
+                  Text(
+                    " and ",
+                    style: bodyTextStyle,
+                  ),
+                  TextButton(
+                    child: Text(
+                      "Ryan Hoover",
+                      style: linkTextStyle,
+                    ),
+                    onPressed: () {
+                      js.context
+                          .callMethod('open', ['https://twitter.com/rrhoover']);
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 100.0, right: 100.0, bottom: 16.0),
+              child: TextField(
+                autofocus: true,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // TODO: add functionality
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "CLEAN",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ url_cleaner/lib/styles.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+// TODO: update all text to use Poppins
+
+TextStyle headerStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 50.0,
+  fontWeight: FontWeight.w700,
+  shadows: <Shadow>[
+    Shadow(
+      offset: Offset(1.0, 1.0),
+      blurRadius: 3,
+      color: Color.fromARGB(255, 0, 0, 0),
+    ),
+  ],
+);
+
+TextStyle linkTextStyle = TextStyle(
+  color: Colors.blue,
+);
+
+TextStyle bodyTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 16.0,
+);
